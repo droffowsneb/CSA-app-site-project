9e3cb51c5fef8e553545f2bd6c9a0e1128e22232
ben <ben.swofford@gmail.com>
2017-12-10 15:12:27 -0500

    farms map backup before trying to fix multiple InfoWindows

--- js/bucket.js
+++ js/bucket.js
@@ -23,12 +23,11 @@ function initMap() {
 
   /*
   // CUSTOM PIN CODE
-  // I can't get this to actually show up. might be a z-index problem?
   var farmPin = {
-    url: '../images/farm-pin-dark.svg',
+    url: '../images/farm-pin-dark.png',
     size: new google.maps.Size(20, 33),
     origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(10, 33)
+    anchor: new google.maps.Point(10, 33),
   };
   */
 
@@ -37,37 +36,70 @@ function initMap() {
     zoom: 8,
   });
 
-  var marker = new google.maps.Marker({
+  // var infowindow = new google.maps.InfoWindow();
+
+  var marker1 = new google.maps.Marker({ // RCO
     map: map,
     position: riverCity,
     title: 'River City Outpost'
   });
 
-  var marker = new google.maps.Marker({
+  var marker2 = new google.maps.Marker({ // --------- Castle Valley Mill ---------
     map: map,
     position: castleValley,
     title: 'Castle Valley Mill',
   });
+  var castleValleyInfo = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h3 id="firstHeading" class="firstHeading">Castle Valley Mill</h3>'+
+    '<div id="bodyContent">'+
+    '<p>Located in the heart of Bucks County, Pennsylvania, <b>Castle Valley Mill</b> continues to produce the finest stone ground flours, meals, cleaned whole berry grains, and grain mixes.</p>'+
+    '<p><a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwidg_aQnIDYAhVCTd8KHRv9CiEQFggqMAA&url=http%3A%2F%2Fcastlevalleymill.com%2F&usg=AOvVaw2RzjrBWyQgtEFOgJpHpFle">Learn more »</a></p>'+
+    '</div>'+
+    '</div>';
+  var castleValleyInfoWindow = new google.maps.InfoWindow({
+    content: castleValleyInfo
+  });
+  marker2.addListener('click', function() {
+    castleValleyInfoWindow.open(map, marker2);
+  });
 
-  var marker = new google.maps.Marker({
+
+  var marker3 = new google.maps.Marker({ // --------- Koffman's Fruit Farm ---------
     map: map,
     position: koffmansFruit,
     title: 'Koffman&#39;s Fruit Farm'
   });
+  var koffmansFruitInfo = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h3 id="firstHeading" class="firstHeading">Koffman&#39;s Fruit Farm</h3>'+
+    '<div id="bodyContent">'+
+    '<p>Kauffman&#39;s Family Orchard products have been recognized and enjoyed since the early 1900&#39;s. Founder Amos L. Kauffman planted the first fruit trees in 1911, and today the 3rd, 4th, & 5th generations own & operate the business.</p>'+
+    '<p><a href="https://www.kauffmansfruitfarm.com/">Learn more »</a></p>'+
+    '</div>'+
+    '</div>';
+  var koffmansFruitInfoWindow = new google.maps.InfoWindow({
+    content: koffmansFruitInfo
+  });
+  marker3.addListener('click', function() {
+    koffmansFruitInfoWindow.open(map, marker3);
+  });
 
-  var marker = new google.maps.Marker({
+  var marker4 = new google.maps.Marker({
     map: map,
     position: maplehofe,
     title: 'Maplehofe Dairy Farm Store'
   });
 
-  var marker = new google.maps.Marker({
+  var marker5 = new google.maps.Marker({
     map: map,
     position: soomFoods,
     title: 'Soom Foods'
   });
 
-  var marker = new google.maps.Marker({
+  var marker6 = new google.maps.Marker({
     map: map,
     position: daisyFlour,
     title: 'Daisy Flour'
